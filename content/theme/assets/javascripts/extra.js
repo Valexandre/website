278c23f8ac80c70305d209cfcceff93d87b89965
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-13 20:24:12 +0800

    Delete content directory

--- content/theme/assets/javascripts/extra.js
+++
@@ -1,8 +0,0 @@
-// Open links externally.
-var links = document.links;
-
-for (var i = 0, linksLength = links.length; i < linksLength; i++) {
-    if ((links[i].hostname != window.location.hostname) && !("data-mediabox" in links[i].attributes)) {
-        links[i].target = '_blank';
-    }
-}
